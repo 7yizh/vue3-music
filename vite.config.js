f1eb915f4e388dd2fcd31bbbea61c6a5128d3ae0
mq <1052818452@qq.com>
2026-03-06 17:37:03 +0800

    test3

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import { fileURLToPath, URL } from 'url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',//'/vue3-music/',
+  base: '/vue3-music/',
   plugins: [vue()],
   resolve: {
     alias: {
